6355f7aaa41aba9a7a0ab7fd1a46e12c595fdec2
Takumi Karibe <takumi.k.5610@gmail.com>
2019-07-09 09:29:39 +0900

    remove unnecessary variable

--- main.go
+++ main.go
@@ -15,8 +15,7 @@ func main() {
 
 	if err != nil {
 		color.New(color.FgWhite).Add(color.Bold).Add(color.BgRed).Print("\n ERROR ")
-		fontRed := color.New(color.FgRed).Add(color.Bold)
-		fontRed.Println(" Homebrew is not installed. Please execute the following command to install.\n")
+		color.New(color.FgRed).Add(color.Bold).Println(" Homebrew is not installed. Please execute the following command to install.\n")
 		color.New(color.FgYellow).Add(color.Bold).Println("/usr/bin/ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"")
 		os.Exit(1)
 	}
